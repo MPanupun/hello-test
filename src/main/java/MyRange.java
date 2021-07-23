7498706364b61ea2f2425c12e024d18b615547fa
M Panupun <panupun.see@ngernturbo.com>
2021-07-23 08:18:54 +0000

    change models

--- src/main/java/MyRange.java
+++ src/main/java/MyRange.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class MyRange {
     public static final int ASCII_VALUE_OF_1 = 48;
     public final String input;
@@ -10,9 +12,23 @@ public class MyRange {
         return  this.input.startsWith("[");
     }
     public int getStart() {
-        if(isStartWithInclude()) {
-            return this.input.charAt(1) - ASCII_VALUE_OF_1;
-        }
-        return  -1;
+        int StartNumber = Integer.parseInt(String.valueOf(this.input.charAt(1)));
+        if (isStartWithInclude())
+            return StartNumber;
+        return totalStartNumber(StartNumber ,+ 1);
+    }
+    public boolean isEndWithExclude() {
+        return  this.input.endsWith("]");
+    }
+
+    public int getEnd() {
+        int StartNumber = Integer.parseInt(String.valueOf(this.input.charAt(3)));
+        if (isEndWithExclude())
+            return StartNumber;
+        return totalStartNumber(StartNumber ,- 1);
+    }
+    public int totalStartNumber(int StartNumber,int num)
+    {
+        return  StartNumber + num;
     }
 }
